4097f8656af558ee81fe64ef8660f50f6fa7f8f7
AdarshKumar9002 <148328407+AdarshKumar9002@users.noreply.github.com>
2024-01-28 17:33:24 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,5 +1,9 @@
 // eslint-disable-next-line import/extensions
-import info from './local.js';
+const info = {
+    name: "Adarsh Kumar",
+    degination: "Front-End Devloper",
+    description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. "
+};
 
 // Home page
 const HAMBURGER_MENU_ELEMENT = document.getElementById('btn-menu');
