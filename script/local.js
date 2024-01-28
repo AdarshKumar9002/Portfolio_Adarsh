66c5be4477f664c046b029861f72cbbd0b57f339
AdarshKumar9002 <148328407+AdarshKumar9002@users.noreply.github.com>
2024-01-28 22:00:44 +0530

    Update local.js

--- script/local.js
+++ script/local.js
@@ -4,5 +4,17 @@ const info = {
     degination: "Front-End Devloper",
     description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. "
 };
+const errorMsg = { 
+    validName: "Enter a valid Name. ",
+    firstNamelength:"First Name should be between 2 and 20 characters. ",
+    firstNameContainSpace:"first name should not have space in it",
+    lastNamelength:"Last Name should be between 2 and 20 characters. ",
+    lastNameContainSpace:"Last name should not have space in it",
+    emailContainSpace: "email should not Contain space. ",
+    validEmail: "Enter a valid email id. ",
+    numberContainSpace: "Phone Number should not Contain space. ",
+    validNumber: "Enter a valid Phone number. "
+    
+};
 
-export default info ;
+export {info , errorMsg};
